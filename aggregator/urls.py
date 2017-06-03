1fa32a8be9a67b29800c54a91adb0b06fc416f3c
anirudhshenoy <anirudhshenoy92@gmail.com>
2017-06-04 02:29:30 +0530

    changed form method to GET, integrated front page to chapter view

--- aggregator/urls.py
+++ aggregator/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #url(r'^$', views.IndexView.as_view(), name="index"),
     url(r'^$', views.index, name="index"),
+    url(r'^chapter/$', views.IndexView.as_view(), name="chapter"),
+    #url(r'^chapter/$', views.chapter, name="chapter"),
 ]
--- aggregator/views.py
+++ aggregator/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views import generic
+
+
 from .models import content
 
 # Create your views here.
@@ -10,8 +12,25 @@ def index(request):
     return render(request, 'aggregator/index.html', context)
 
 
+def chapter(request):
+    return HttpResponse("{% csrf_token %}<h3>Hello!</h3>")
+
+
 class IndexView(generic.ListView):
+    model = content
     template_name = 'aggregator/index2.html'
 
+    # def post(self, request, *args, **kwargs):
+    #     gradeRequest = request.POST.get('grade')
+    #     subjectRequest = request.POST.get('subject')
+    #     return HttpResponse(gradeRequest + ' ' + subjectRequest)
+
+        #return HttpResponse(grade + ' ' + subject)
+        #return render(request, self.template_name, {'gradeRequest': gradeRequest, 'subjectRequest': subjectRequest})
+
     def get_queryset(self):
-        return content.objects.all().filter(grade='5th')
+        queryset = content.objects.all()
+
+        if self.request.GET.get('grade'):
+            queryset = queryset.filter(grade=self.request.GET.get('grade'))
+        return queryset
